911cbe64d1714b4218cda23992b692f8dcf444f5
Ari Anisfeld <anisfeld@uchicago.edu>
2019-07-15 14:07:50 -0500

    create user account (UC01a-B)
    * #16 basic API functional
    
    * #16 token auth and first_name last_name
    
    * #16 raise error on bad login
    
    * #16 account and authorization tests

---
+++ backend/accounts/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'first_name', 'last_name')
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password', 'first_name', 'last_name')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if not (user and user.is_active):
+            raise serializers.ValidationError("We can't find that username and password. You can reset your password or try again.")
+        return user
