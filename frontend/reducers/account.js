a8b0b4827696c08a20b4548454188e31d0644857
Hongji Liu <h125678@gmail.com>
2019-09-03 00:27:33 +0800

    almost finished

--- frontend/reducers/account.js
+++ frontend/reducers/account.js
@@ -8,13 +8,22 @@ import {
   PAYMENT_SUCCESS,
   PAYMENT_FAILED,
   WITHDRAW_FAILED,
-  WITHDRAW_SUCCESS
+  WITHDRAW_SUCCESS,
+  TRANSACTIONS_LOADED,
+  TRANSACTIONS_FAILED,
+  APPROVE_FAIL,
+  APPROVE_SUCCESS,
+  REJECT_FAIL,
+  REJECT_SUCCESS,
+  CANCEL_FAIL,
+  CANCEL_SUCCESS
 } from "../constants/Types";
 
 const initialState = {
   circleAccount: null,
   userAccount: null,
-  isLoading: null
+  isLoading: null,
+  transactions: null
 }
 
 export default function(state = initialState, action) {
@@ -66,6 +75,54 @@ export default function(state = initialState, action) {
         isLoading: false
       };
     case WITHDRAW_FAILED:
+      Alert.alert(action.payload);
+      return {
+        ...state,
+        isLoading: false
+      };
+    case TRANSACTIONS_LOADED:
+      return {
+        ...state,
+        isLoading: false,
+        transactions: action.payload
+      };
+    case TRANSACTIONS_FAILED:
+        return {
+          ...state,
+          isLoading: false,
+          transactions: action.payload
+    };
+    case APPROVE_SUCCESS:
+      Alert.alert('Approved!');
+      return {
+        ...state,
+        isLoading: false
+      };
+    case APPROVE_FAIL:
+      Alert.alert(action.payload);
+      return {
+        ...state,
+        isLoading: false
+      };
+    case REJECT_SUCCESS:
+      Alert.alert('Rejected!');
+      return {
+        ...state,
+        isLoading: false
+      };
+    case REJECT_FAIL:
+      Alert.alert(action.payload);
+      return {
+        ...state,
+        isLoading: false
+      };
+    case CANCEL_SUCCESS:
+      Alert.alert('Cancel!');
+      return {
+        ...state,
+        isLoading: false
+      };
+    case CANCEL_FAIL:
       Alert.alert(action.payload);
       return {
         ...state,
